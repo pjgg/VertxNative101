823b186678fd1966bd4d787e7c6364588df442f3
pablo gonzalez granados <pagonzal@redhat.com>
2021-07-10 22:09:56 +0200

    init commit

---
+++ src/main/java/org/acme/HTTPVerticle.java
@@ -0,0 +1,23 @@
+package org.acme;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class HTTPVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello!");
+    }).listen(8888, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
